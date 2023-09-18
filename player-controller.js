1d8db6f7697f65364d90514b34a73613e13af538
DONGHAEY <dongheaey@gmail.com>
2023-09-18 17:37:53 +0900

    CHANGE PLATFORM CHROMIUM

--- player-controller.js
+++ player-controller.js
@@ -1,21 +1,20 @@
 const { Builder } = require("selenium-webdriver");
-const chrome = require("selenium-webdriver/chrome");
+const chromium = require("selenium-webdriver/chromium");
 const { exec } = require("shelljs");
 const path = require("path");
 const SERVER_END_POINT = require("./SERVER");
 
 module.exports = async function settingPlayer(playerSocket) {
   let driver = null;
-  const BROWSER_OPTIONS = new chrome.Options();
+  const BROWSER_OPTIONS = new chromium.Options();
   BROWSER_OPTIONS.addArguments("--autoplay-policy=no-user-gesture-required");
-  const BROWSER_PLATFORM = "chrome";
-
+  const BROWSER_PLATFORM = "chromium";
   exec(`/usr/bin/amixer -D pulse sset Master 100%`);
 
   playerSocket.on("connect", async () => {
     driver = new Builder();
     driver.forBrowser(BROWSER_PLATFORM);
-    driver.setChromeOptions(BROWSER_OPTIONS);
+    driver.setchromiumOptions(BROWSER_OPTIONS);
     driver = await driver.build();
     const playerUrl = path.join(__dirname, "player.html");
     await driver.get(`file://${playerUrl}?server_url=${SERVER_END_POINT}`);
@@ -25,6 +24,7 @@ module.exports = async function settingPlayer(playerSocket) {
     if (driver) {
       await driver?.quit();
     }
+    driver = null;
   });
 
   playerSocket.connect();
