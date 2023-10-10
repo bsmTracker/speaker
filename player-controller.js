8d4b24877250a0aa731728eb42639446603f06a9
DONGHAEY <dongheaey@gmail.com>
2023-10-10 17:05:22 +0900

    UPDATE

--- player-controller.js
+++ player-controller.js
@@ -7,16 +7,15 @@ let browser = null;
 
 // executablePath: '/path/to/Chrome'
 module.exports = async function settingPlayer(playerSocket) {
-  exec(`/usr/bin/amixer -D pulse sset Master 100%`);
-
   playerSocket.on("connect", async () => {
+    exec(`/usr/bin/amixer -D pulse sset Master 100%`);
     if (browser) {
       await browser.close();
     }
     const playerUrl = path.join(__dirname, "player.html");
     browser = await puppeteer.launch({
       headless: false,
-      executablePath: "/path/to/Chrome",
+      executablePath: "/snap/bin/chromium",
       args: ["--autoplay-policy=no-user-gesture-required"],
     });
     const page = await browser.newPage();
