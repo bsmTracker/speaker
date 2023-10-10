95fbd5bd824d56d2553fd041b91af22b894719f3
DONGHAEY <dongheaey@gmail.com>
2023-10-10 17:12:31 +0900

    edit start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-. /home/myapp/.nvm/nvm.sh
-/home/speaker/.nvm/versions/node/v18.13.0/bin/node speaker.js
+/home/speaker/.nvm/versions/node/v18.13.0/bin/node /home/speaker/Desktop/speaker-player/index.js
